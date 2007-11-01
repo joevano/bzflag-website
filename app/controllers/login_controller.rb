ea6e86c25fb96b3d143ed114ddeba19c13b74a84
Bernt Hansen <bernt@norang.ca>
2007-11-01 10:02:05 -0400

    Refactor login setting session variables: userid bzid groups
    Display the response result and parse values to a table

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -11,28 +11,34 @@ class LoginController < ApplicationController
   def validate
     @username = params[:username]
     @token = params[:token]
-    groups = ['NORANG.HIDE','NORANG.RECORD','NORANG.REPLAY','NORANG.JRADMIN','NORANG.SRADMIN','NORANG.TRADMIN','DEVELOPERS']
-    site = "/db/?action=CHECKTOKENS&checktokens=#{@username}%3D#{@token}&groups=" + groups.join('%0D%0A')
-    @response = Net::HTTP.get('my.bzflag.org', site)
 
-    valid_login = nil
-    @username = nil
-    @groups = []
-    @bzid = nil
-    for line in @response
-      if line.index('TOKGOOD: ')
-        data = line.split(' ',2)[1]
-        @username = data.split(':',2)[0]
-        @groups = data.split(':',2)[1].split(':')
-        valid_login = true
-      elsif line.index('BZID: ')
-        @bzid = line.split(' ')[1]
+    groups = ['NORANG.HIDE',
+              'NORANG.RECORD',
+              'NORANG.REPLAY',
+              'NORANG.JRADMIN',
+              'NORANG.SRADMIN',
+              'NORANG.TRADMIN',
+              'DEVELOPERS']
+    checktoken = "/db/?action=CHECKTOKENS&checktokens=#{@username}%3D#{@token}&groups=" + groups.join('%0D%0A')
+    @response = Net::HTTP.get('my.bzflag.org', checktoken)
+
+    session[:username] = nil
+    session[:bzid] = nil
+    session[:groups] = []
+    if @response.index('TOKGOOD: ')
+      for line in @response
+        if line.index('TOKGOOD: ')
+          data = line.split(' ',2)[1]
+          session[:username] = data.split(':',2)[0]
+          session[:groups] = data.split(':',2)[1].split(':')
+        elsif line.index('BZID: ')
+          session[:bzid] = line.split(' ')[1]
+        end
       end
-    end
-    if valid_login
       flash[:notice] = "Login successful!"
     else
       flash[:notice] = "Login failed - bad token"
     end
+
   end
 end
