885a079cf8383684743eed85c9b455171980441c
Bernt Hansen <bernt@norang.ca>
2007-11-07 02:48:00 -0500

    Add stub views

--- app/controllers/bzflag_controller.rb
+++ app/controllers/bzflag_controller.rb
@@ -7,11 +7,33 @@ class BzflagController < ApplicationController
   def servers
   end
 
+  def help
+  end
+
+  def reports
+  end
+
+  def players
+  end
+
+  def bans
+  end
+
+  def recordings
+  end
+
+  def admins
+  end
+
+  def mailing_list
+  end
+
   private
 
   def get_user
     @userid = session[:bzid]
     @username = session[:username]
+    @admin = session[:admin]
   end
 
 end
