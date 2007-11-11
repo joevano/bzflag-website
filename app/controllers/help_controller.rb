c0fbda51638e11fe4f05956891de9a5988216560
Bernt Hansen <bernt@norang.ca>
2007-11-11 11:27:34 -0500

    Add help controller

---
+++ app/controllers/help_controller.rb
@@ -0,0 +1,24 @@
+require 'fileutils'
+
+class HelpController < ApplicationController
+  before_filter :get_user
+
+  def method_missing(action)
+    if action == "index"
+      action = "bzflag-admin-help2"
+    end
+
+    @action = action
+    file = "help/#{action}.html"
+    begin
+      @content = IO.read(file)
+      # Strip off the header
+      @content = @content.sub(/^.*<body class="crock">/im, '')
+      # Strip off the trailer
+      @content = @content.sub(/<\/body>.*$/im, '')
+    rescue
+      flash[:notice] = "Can't find help page: #{action}"
+    end
+    render :template => "mailing_list/article", :layout => "bzflag"
+  end
+end
---
+++ test/functional/help_controller_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class HelpControllerTest < ActionController::TestCase
+  tests HelpController
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
