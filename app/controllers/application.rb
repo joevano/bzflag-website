4d1acc627270560d51e0ca1fcdca5ef0599f30e5
Bernt Hansen <bernt@norang.ca>
2007-11-12 10:06:12 -0500

    Add configuration item for TRADMINS

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -20,25 +20,25 @@ class ApplicationController < ActionController::Base
     @sradmin = is_sradmin()
     @tradmin = is_tradmin()
     @hideadmin = is_hideadmin()
-    @admin = @jradmin or @sradmin or @tradmin or @hideadmin
+    @admin = @jradmin || @sradmin || @tradmin || @hideadmin
     @groups = session[:groups]
     @ip = session[:ip]
   end
 
   def is_hideadmin
-    session[:groups] and session[:groups].index("NORANG.HIDE")
+    session[:groups] and !session[:groups].index("NORANG.HIDE").nil?
   end
 
   def is_jradmin
-    session[:groups] and session[:groups].index("NORANG.JRADMIN")
+    session[:groups] and !session[:groups].index("NORANG.JRADMIN").nil?
   end
 
   def is_sradmin
-    session[:groups] and session[:groups].index("NORANG.SRADMIN")
+    session[:groups] and !session[:groups].index("NORANG.SRADMIN").nil?
   end
 
   def is_tradmin
-    session[:groups] and session[:groups].index("NORANG.TRADMIN")
+    session[:groups] and !session[:groups].index("NORANG.TRADMIN").nil?
   end
 
   def read_html_and_strip_to_body_content(file)
