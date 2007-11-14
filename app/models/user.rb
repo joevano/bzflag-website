c430f901c2f3e361e67fc61cbb5f55c9abdcaa94
Bernt Hansen <bernt@norang.ca>
2007-11-14 00:32:18 -0500

    Lookup permissions for users through groups and collect names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   validates_uniqueness_of :bzid
   has_and_belongs_to_many :groups
+
+  def permissions
+    perms = []
+    self.groups.each do |g|
+      perms = perms | g.permissions
+    end
+    perms
+  end
 end
