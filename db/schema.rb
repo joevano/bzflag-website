3254a93b4cec19f8e858a73f85341859d7a317f0
Bernt Hansen <bernt@norang.ca>
2007-11-15 00:14:46 -0500

    Add Permissions as a database migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 15) do
+ActiveRecord::Schema.define(:version => 16) do
 
   create_table "bz_servers", :force => true do |t|
     t.datetime "created_at"
