974fb5479d3ed9d0bc3fa5236daac1bc920e5df5
Bernt Hansen <bernt@norang.ca>
2007-11-20 21:03:28 -0500

    Use configuration items for website content

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,10 +1,18 @@
 require 'net/http'
+require 'ostruct'
+require 'yaml'
+
+config = OpenStruct.new(YAML.load_file("#{RAILS_ROOT}/config/config.yml"))
+env_config = config.send(RAILS_ENV)
+config.common.update(env_config) unless env_config.nil?
+::AppConfig = OpenStruct.new(config.common)
 
 class LoginController < ApplicationController
 
   layout "bzflag"
 
   def login
+    @host = AppConfig.root_url
   end
 
   def logout
