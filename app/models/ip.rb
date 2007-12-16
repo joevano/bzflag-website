e2890823711484cac95a62259722b0a2b69b6d3f
Bernt Hansen <bernt@norang.ca>
2007-12-16 14:41:32 -0500

    Do a reverse lookup of the hostname when a new IP is encountered

--- app/models/ip.rb
+++ app/models/ip.rb
@@ -3,6 +3,15 @@ class Ip < ActiveRecord::Base
   has_many :callsigns, :through => :player_connections
 
   def self.locate(ip)
-    Ip.find_by_ip(ip) || Ip.create(:ip => ip)
+    newip = Ip.find_by_ip(ip)
+    if newip.nil?
+      newip = Ip.create!(:ip => ip)
+      begin
+        newip.hostname = Socket.gethostbyaddr(ip.split(".").collect{|y| y.to_i}.pack("CCCC"))[0]
+      rescue
+      end
+      newip.save!
+    end
+    return newip
   end
 end
