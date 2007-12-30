156a3907d2be04f455240a62bd02636394442ece
Bernt Hansen <bernt@norang.ca>
2007-12-30 18:11:09 -0500

    Add log_parser unit test

---
+++ test/unit/log_parser_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + '/../../lib/log_parser'
+
+class LogParserTest < Test::Unit::TestCase
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
