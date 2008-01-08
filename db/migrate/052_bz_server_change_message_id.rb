863ec0f06344117dc340cfcd356a03419a6f941e
Bernt Hansen <bernt@norang.ca>
2008-01-08 10:12:47 -0500

    Fix links to messages
    Remove the old junk

--- db/migrate/052_bz_server_change_message_id.rb
+++ db/migrate/052_bz_server_change_message_id.rb
@@ -3,9 +3,15 @@ class BzServerChangeMessageId < ActiveRecord::Migration
     add_column :bz_servers, :last_chat_message_id, :integer
     add_column :bz_servers, :last_filtered_message_id, :integer
     add_column :bz_servers, :server_status_message_id, :integer
+    remove_column :bz_servers, :last_chat_log_message_id
+    remove_column :bz_servers, :last_filtered_log_message_id
+    remove_column :bz_servers, :server_status_log_message_id
   end
 
   def self.down
+    add_column :bz_servers, :last_chat_log_message_id, :integer
+    add_column :bz_servers, :last_filtered_log_message_id, :integer
+    add_column :bz_servers, :server_status_log_message_id, :integer
     remove_column :bz_servers, :last_chat_message_id
     remove_column :bz_servers, :last_filtered_message_id
     remove_column :bz_servers, :server_status_message_id
