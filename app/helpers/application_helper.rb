fdf016712db97888e4a4a99370120bc47065911c
Bernt Hansen <bernt@norang.ca>
2008-01-11 23:07:26 -0500

    Add time format helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def display_date(date)
+    date && date.strftime("%Y-%m-%dT%H:%M:%SZ")
+  end
 end
