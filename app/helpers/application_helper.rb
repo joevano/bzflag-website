7ae8e94ec0437b3239ee029d4b0cf46413f1ba72
Bernt Hansen <bernt@norang.ca>
2008-01-11 23:17:58 -0500

    Add helper for displaying date ranges and use it for player search results

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,21 @@ module ApplicationHelper
   def display_date(date)
     date && date.strftime("%Y %b %d %H:%M:%S")
   end
+
+  def display_date_range(d1, d2)
+    range = display_date(d1)
+    if d2
+      range = range + " - "
+      if d2.strftime("%Y") != d1.strftime("%Y")
+        range = range + display_date(d2)
+      elsif d2.strftime("%b") != d1.strftime("%b")
+        range = range + d2.strftime("%b %d %H:%M:%S")
+      elsif d2.strftime("%d") != d1.strftime("%d")
+        range = range + d2.strftime("%d %H:%M:%S")
+      else
+        range = range + d2.strftime("%H:%M:%S")
+      end
+    end
+    range
+  end
 end
