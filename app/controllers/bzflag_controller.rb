7c7e6015b93c99caee582238bef5493de584de13
Bernt Hansen <bernt@norang.ca>
2008-03-16 01:23:59 -0400

    Whitespace

--- app/controllers/bzflag_controller.rb
+++ app/controllers/bzflag_controller.rb
@@ -2,7 +2,9 @@ class BzflagController < ApplicationController
   before_filter :authorize_admin_menu_perm, :except => [ :index ]
 
   def index
-    @bz_servers = BzServer.find(:all, :order => "current_players_count desc, log_messages.logged_at desc, server_host_id, port", :include => ["last_chat_message", "last_filtered_message", "current_players"])
+    @bz_servers = BzServer.find(:all,
+                                :order => "current_players_count desc, log_messages.logged_at desc, server_host_id, port",
+                                :include => ["last_chat_message", "last_filtered_message", "current_players"])
   end
 
   def help
@@ -10,7 +12,9 @@ class BzflagController < ApplicationController
 
   def reports
     msg_report_id = LogType.find_by_token("MSG-REPORT").id
-    @reports = LogMessage.find(:all, :conditions => "log_type_id = #{msg_report_id}", :order => "logged_at desc", :limit => 100)
+    @reports = LogMessage.find(:all,
+                               :conditions => "log_type_id = #{msg_report_id}",
+                               :order => "logged_at desc", :limit => 100)
   end
 
   def bans
