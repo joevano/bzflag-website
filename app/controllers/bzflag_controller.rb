f0953f8dba6f1981af4c4e4799819def21bb149a
Bernt Hansen <bernt@norang.ca>
2008-03-17 01:16:14 -0400

    server-list: Add recent bans and recent reports

--- app/controllers/bzflag_controller.rb
+++ app/controllers/bzflag_controller.rb
@@ -10,12 +10,22 @@ class BzflagController < ApplicationController
       @total_players += b.current_players_count
       @total_running_servers += 1 if b.server_status_message && b.server_status_message.message.text.capitalize == 'Running'
     end
-    
+
+    @recent_ban_days = 3
+    command_type_id = LogType.find_by_token("MSG-COMMAND").id
+    @recent_bans = LogMessage.find(:all, 
+                                   :conditions => "log_type_id = #{command_type_id} and logged_at > '#{db_date(@recent_ban_days.days.ago)}' and (text like 'ban %' or text like 'unban %' or text like 'hostban %' or text like 'hostunban %' or text like 'idban %' or text like 'idunban %' or text like 'poll ban %')", 
+                                   :order => "logged_at desc",
+                                   :include => "message")
+
+    report_type_id = LogType.find_by_token("MSG-REPORT").id
+    @recent_report_days = 3
+    @recent_reports = LogMessage.find(:all, :conditions => "log_type_id = #{report_type_id} and logged_at > '#{db_date(@recent_report_days.days.ago)}'", :order => "logged_at desc")
   end
 
   def logs
     @bz_server = BzServer.find(params[:id])
-    @log_messages = @bz_server.log_messages.find(:all, :conditions => "logged_at > '#{24.hours.ago.strftime("%Y-%m-%d %H:%M:%S")}'")
+    @log_messages = @bz_server.log_messages.find(:all, :conditions => "logged_at > '#{db_date(24.hours.ago)}'")
   end
 
   def help
