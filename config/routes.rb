c60f6428ada668eacd2a725177464e7b48a6113b
Joe Van Overberghe <joevano@gmail.com>
2009-06-06 20:55:18 -0400

    Remove the wsdl action from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -31,10 +31,6 @@ ActionController::Routing::Routes.draw do |map|
   # -- just remember to delete public/index.html.
   map.connect '', :controller => "bzflag"
 
-  # Allow downloading Web Service WSDL as a file with an extension
-  # instead of a file named 'wsdl'
-  map.connect ':controller/service.wsdl', :action => 'wsdl'
-
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action.:format'
