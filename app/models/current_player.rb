f2b0ed6dcc088b10c9c3d5f056e93c65abbcc96e
Bernt Hansen <bernt@norang.ca>
2009-06-06 22:53:19 -0400

    Add LGPL v2.1 license information to all files

--- app/models/current_player.rb
+++ app/models/current_player.rb
@@ -1,3 +1,22 @@
+#-------------------------------------------------------------------------------
+# The BZFlag Website Project - administration and monitoring of BZFlag servers
+# Copyright (C) 2009  Bernt T. Hansen
+#
+# This website project is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This website project is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this website project; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#-------------------------------------------------------------------------------
+
 class CurrentPlayer < ActiveRecord::Base
   validates_presence_of :bz_server_id, :callsign
   belongs_to :bz_server, :counter_cache => true
