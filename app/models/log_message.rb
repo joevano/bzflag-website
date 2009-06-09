54f71a37667c5c36b246615cb22e1ea71a4e57e2
Joe Van Overberghe <joevano@gmail.com>
2009-06-09 19:41:36 -0400

    Add db_date back to handle possible UTC time issues
    Extended ActiveRecord::Base with the custom method

--- app/models/log_message.rb
+++ app/models/log_message.rb
@@ -33,14 +33,14 @@ class LogMessage < ActiveRecord::Base
 
   def self.recent_ban_days
     find(:all,
-         :conditions => "log_type_id = #{LogType.find_by_token("MSG-COMMAND").id} and logged_at > '#{@recent_ban_days.days.ago}' and (text like 'ban %' or text like 'unban %' or text like 'hostban %' or text like 'hostunban %' or text like 'idban %' or text like 'idunban %' or text like 'poll ban %')",
+         :conditions => "log_type_id = #{LogType.find_by_token("MSG-COMMAND").id} and logged_at > '#{db_date(@recent_ban_days.days.ago)}' and (text like 'ban %' or text like 'unban %' or text like 'hostban %' or text like 'hostunban %' or text like 'idban %' or text like 'idunban %' or text like 'poll ban %')",
          :order => "logged_at",
          :joins => :message,
          :include => "message")
   end
 
   def self.recent_report_days
-    find(:all, :conditions => "log_type_id = #{LogType.find_by_token("MSG-REPORT").id} and logged_at > '#{@recent_report_days.days.ago}'", :order => "logged_at")
+    find(:all, :conditions => "log_type_id = #{LogType.find_by_token("MSG-REPORT").id} and logged_at > '#{db_date(@recent_report_days.days.ago)}'", :order => "logged_at")
   end
   
 end
--- config/environment.rb
+++ config/environment.rb
@@ -63,3 +63,5 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+require 'custom_active_record_methods'
