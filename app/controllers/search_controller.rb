a45781b1e6542d12f38f08186901d1ed65891f34
Joe Van Overberghe <joevano@gmail.com>
2009-06-13 19:14:25 -0400

    Tweak log search to gain speed
    Remove the 'logged_at desc' from the order clause as it
    was redundant and made the generated query unable to use
    any of the existing indexes. Log_messages.id are created
    in the same order as the messages are in the raw logs
    which technically should be in the same order as 'logged_at'.
    The id field is indexed which is where the speed boost
    comes from. Removal increased performance of the log
    search from ~9000ms to ~550ms from click to render.
    
     Signed-off-by: Joe Van Overberghe <joevano@gmail.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -81,7 +81,7 @@ class SearchController < ApplicationController
     if next_page
         @log_messages = @bz_server.log_messages.find(:all,
                                                      :conditions => "log_messages.id >= #{next_page} and log_type_id in (1,2,3,4,5,6,7,8,9,10,11,12,13)", 
-                                                     :order => "logged_at, log_messages.id",
+                                                     :order => "log_messages.id",
                                                      :include => [ "message", "callsign", "to_callsign" ],
                                                      :limit => 100)
         if @log_messages.empty?
@@ -90,7 +90,7 @@ class SearchController < ApplicationController
     elsif prev_page
         @log_messages = @bz_server.log_messages.find(:all,
                                                      :conditions => "log_messages.id <= #{prev_page} and log_type_id in (1,2,3,4,5,6,7,8,9,10,11,12,13)", 
-                                                     :order => "logged_at desc, log_messages.id desc",
+                                                     :order =>  "log_messages.id desc",
                                                      :include => [ "message", "callsign", "to_callsign" ],
                                                      :limit => 100).reverse
     end
@@ -98,7 +98,7 @@ class SearchController < ApplicationController
     if @log_messages.empty?
         @log_messages = @bz_server.log_messages.find(:all,
                                                      :conditions => "log_type_id in (1,2,3,4,5,6,7,8,9,10,11,12,13)", 
-                                                     :order => "logged_at desc, log_messages.id desc",
+                                                     :order => "log_messages.id desc",
                                                      :include => [ "message", "callsign", "to_callsign" ],
                                                      :limit => 100).reverse
     end
