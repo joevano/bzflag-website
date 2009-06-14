20ba17c742b161d84f6f4435345e212142098f63
Joe Van Overberghe <joevano@gmail.com>
2009-06-14 19:23:53 -0400

    Create named_scope :callsign_like
    Added functionality to the Callsign model that allows for searching
    for callsigns based on the sql like keyword.
    
    Signed-off-by: Joe Van Overberghe <joevano@gmail.com>

--- app/models/callsign.rb
+++ app/models/callsign.rb
@@ -18,11 +18,14 @@
 #-------------------------------------------------------------------------------
 
 class Callsign < ActiveRecord::Base
-  validates_presence_of :name
   has_many :player_connections
   has_many :ips, :through => :player_connections, :uniq => true
   has_many :teams, :through => :player_connections, :uniq => true
   has_many :log_messages
+
+  named_scope :callsign_like, lambda {|callsign| { :conditions => ['name like ?', callsign] }}
+
+  validates_presence_of :name
 
   def self.locate(name)
     if name == '' || name =~ /^\s+$/
