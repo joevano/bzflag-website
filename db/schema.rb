c15ff700fc52fc400453b1a8be26d844c540ac85
Bernt Hansen <bernt@norang.ca>
2009-06-28 18:13:56 -0400

    Add callsign SERVER
    The log display assumes there is a SERVER callsign defined and looks
    up the SERVER callsign id.
    
    Signed-off-by: Bernt Hansen <bernt@norang.ca>

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090624193243) do
+ActiveRecord::Schema.define(:version => 20090628220347) do
 
   create_table "bz_servers", :force => true do |t|
     t.datetime "created_at"
